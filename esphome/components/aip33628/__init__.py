170760e5d58a4d5486d408eb2d208d9e34eab27c
ja-lab <59612111+ja-lab@users.noreply.github.com>
2026-09-23 10:16:35 +0100

    Update __init__.py

--- esphome/components/aip33628/__init__.py
+++ esphome/components/aip33628/__init__.py
@@ -6,6 +6,7 @@ from esphome.const import CONF_ID, CONF_NUMBER, PLATFORM_ESP32
 
 CODEOWNERS = ["@misterblack1"]
 ESP_PLATFORMS = [PLATFORM_ESP32]
+AUTO_LOAD = ["light"]
 
 aip33628_ns = cg.esphome_ns.namespace("aip33628")
 Aip33628Panel = aip33628_ns.class_("Aip33628Panel", cg.Component)
